2aca144886a88b58c61696cb6f56b35a57469465
Jason Awbrey <jason.s.awbrey@gmail.com>
2016-03-04 14:22:44 -0800

    Add initial file setup

---
+++ src/Class.php
@@ -0,0 +1,14 @@
+<?php 
+	 class Name
+		{
+		private property;
+
+		 function getProperty(){
+			return $this->property;
+			}
+
+		function setProperty(){
+			$this->property = $property;
+		}
+	}
+ ?>
