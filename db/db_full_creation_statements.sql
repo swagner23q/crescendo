bc5b117c435a73fd2a7a6d63ff475df8f1d55459
Jason Awbrey <jason.s.awbrey@gmail.com>
2016-03-06 14:56:31 -0800

    Created full DB creation statement document and tested statements

--- db/db_full_creation_statements.sql
+++ db/db_full_creation_statements.sql
@@ -1,11 +1,5 @@
 CREATE DATABASE crescendo;
-
-/*
-
-                                            Users table
-
-*/
-
+USE crescendo;
 CREATE TABLE `users` (
 `id` SERIAL PRIMARY KEY,
 `f_name` VARCHAR(255),
@@ -24,62 +18,23 @@ CREATE TABLE `users` (
 `bill_state` VARCHAR(255),
 `bill_postal` VARCHAR(255)
 );
-
 INSERT INTO `users` (`f_name`, `l_name`, `email`, `phone`, `password`, `ship_street`, `ship_apt`, `ship_city`, `ship_state`, `ship_postal`, `bill_street`, `bill_apt`, `bill_city`, `bill_state`, `bill_postal`) VALUES ("Jason", "Awbrey", "jason.s.awbrey@gmail.com", "503-939-9407", "password", "123 Test Street", "APT 32", "Portland", "OR", "97212", "123 Test Streeet", "APT 32", "Portland", "OR", "97212")
-
-
-
-
-/*
-
-                                            Orders table
-
-*/
-
-
 CREATE TABLE `orders` (
   `id` SERIAL PRIMARY KEY,
   `user_id` INTEGER,
   `ship_type` INT,
   `date` DATE
 );
-
-
-
-/*
-
-                                            Shipping Types table
-
-*/
 CREATE TABLE `shipping_types` (
     `id` SERIAL PRIMARY KEY,
     `type` VARCHAR(255)
 );
-
 INSERT INTO shipping_types (type) VALUES ("Free Shipping"), ("Ground"), ("2 Day"), ("Overnight");
-
-
-/*
-
-                                            Product Types table
-
-*/
-
-
 CREATE TABLE `product_types` (
     `id` SERIAL PRIMARY KEY,
     `type` VARCHAR(255)
 );
-
 INSERT INTO `product_types` (type) VALUES ("shirt"), ("pants"), ("shoes"), ("jacket");
-
-
-/*
-
-                                            Products table
-
-*/
-
 CREATE TABLE `products` (
   `id` SERIAL PRIMARY KEY,
   `type_id` INT,
@@ -87,16 +42,7 @@ CREATE TABLE `products` (
   `description` VARCHAR(255),
   `img` VARCHAR(255)
 );
-
 INSERT INTO `products` (type_id, name, description, img) VALUES (1, "Blue Shirt", "This is a stylish shirt!", "~/crescendo/assets/img/blue_shirt.jpg"), (2, "Blue Pants", "These are some stylist pants!", "~/crescendo/assets/img/blue_pants.jpg"), (3, "Black Shoes", "These are some neat kicks!", "~/crescendo/assets/img/black_shoes.jpg"), (4, "Red Jacket", "This jacket is totes warm", "~/crescendo/assets/img/red_jacket.jpg");
-
-
-/*
-
-                                            Order Details table
-
-*/
-
 CREATE TABLE `order_details` (
   `id` SERIAL PRIMARY KEY,
   `order_id` INTEGER,
