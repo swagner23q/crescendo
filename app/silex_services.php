96b99aadcb33be063247613ca66f23fd0585cba6
Jason Awbrey <jason.s.awbrey@gmail.com>
2016-03-06 17:30:12 -0800

    Register SwiftMailer Symfony component

--- app/silex_services.php
+++ app/silex_services.php
@@ -1,3 +1,4 @@
 <?php
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
