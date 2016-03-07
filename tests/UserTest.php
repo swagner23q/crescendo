b7b85ff7d7e3389dd1a4463675a34fcf2e843492
Jordan Meier <jbirdmeier@gmail.com>
2016-03-07 13:35:37 -0800

    add non passing test for update method to User class

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -182,6 +182,50 @@
 			$this->assertEquals($test_user, $result);
 		}
 
+		function test_update()
+		{
+			//Arrange
+			$f_name = "Jason";
+			$l_name = "Awbrey";
+			$email = "jason.s.awbrey@gmail.com";
+			$phone = "503-939-9407";
+			$password = "password";
+			$ship_street = "123 Test Street";
+			$ship_apt = "APT 32";
+			$ship_city = "Portland";
+			$ship_state = "OR";
+			$ship_postal = "97212";
+			$bill_street = "123 Test Street";
+			$bill_apt = "APT 32";
+			$bill_city = "Portland";
+			$bill_state = "OR";
+			$bill_postal = "97212";
+			$test_user = new User($f_name, $l_name, $email, $phone, $password, $ship_street, $ship_apt, $ship_city, $ship_state, $ship_postal, $bill_street, $bill_apt, $bill_city, $bill_state, $bill_postal);
+			$test_user->save();
+
+			$new_f_name = "Mojo";
+			$new_l_name = "Jojo";
+			$new_email = "jason.s.awbrey@gmail.com";
+			$new_phone = "503-555-5555";
+			$new_password = "password";
+			$new_ship_street = "1234 Test Street";
+			$new_ship_apt = "APT 12";
+			$new_ship_city = "Portland";
+			$new_ship_state = "OR";
+			$new_ship_postal = "97212";
+			$new_bill_street = "1234 Test Street";
+			$new_bill_apt = "APT 12";
+			$new_bill_city = "Portland";
+			$new_bill_state = "OR";
+			$new_bill_postal = "97212";
+
+			//Act
+			$test_user->update($new_f_name, $new_l_name, $new_email, $new_phone, $new_password, $new_ship_street, $new_ship_apt, $new_ship_city, $new_ship_state, $new_ship_postal, $new_bill_street, $new_bill_apt, $new_bill_city, $new_bill_state, $new_bill_postal);
+
+			//Assert
+			$this->assertEquals(["Mojo", "Jojo", "jason.s.awbrey@gmail.com", "503-555-5555", "password", "1234 Test Street", "APT 12", "Portland", "OR", "97212", "1234 Test Street", "APT 12", "Portland", "OR", "97212"], [$test_user->getFirstName(), $test_user->getLastName(), $test_user->getEmail(), $test_user->getPhone(), $test_user->getPassword(), $test_user->getShipStreet(), $test_user->getShipApt(), $test_user->getShipCity(), $test_user->getShipState(), $test_user->getShipPostal(), $test_user->getBillStreet(), $test_user->getBillApt(), $test_user->getBillCity(), $test_user->getBillState(), $test_user->getBillPostal()]);
+		}
+
 	}
 
 ?>
