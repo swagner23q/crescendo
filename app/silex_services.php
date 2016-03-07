8e3706fa5e122cb1750af57e8b5c1f1a2f6c81b4
Jason Awbrey <jason.s.awbrey@gmail.com>
2016-03-06 17:32:08 -0800

    Enable Request method override

--- app/silex_services.php
+++ app/silex_services.php
@@ -2,3 +2,6 @@
 $app = new Silex\Application();
 $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 $app->register(new Silex\Provider\SwiftmailerServiceProvider());
+
+use Symfony\Component\HttpFoundation\Request;
+Request::enableHttpMethodParameterOverride();
