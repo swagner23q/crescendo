7361707012b326be463a891e3428e977b4d582a8
Jordan Meier <jbirdmeier@gmail.com>
2016-03-07 16:11:56 -0800

    add a post route to save new users and to render a confirmation page

--- app/routes/register.php
+++ app/routes/register.php
@@ -5,7 +5,25 @@
         return $app['twig']->render('register.html.twig');
     });
 
-    // $app->post('/register', function() use ($app)
-    // {
-    //     return $app['twig']->render('register_confirm.html.twig', array());
-    // });
+    $app->post('/register', function() use ($app)
+    {
+        $user = new User(
+            $_POST['f_name'],
+            $_POST['l_name'],
+            $_POST['email'],
+            $_POST['phone'],
+            $_POST['password'],
+            $_POST['ship_street'],
+            $_POST['ship_apt'],
+            $_POST['ship_city'],
+            $_POST['ship_state'],
+            $_POST['ship_postal'],
+            $_POST['bill_street'],
+            $_POST['bill_apt'],
+            $_POST['bill_city'],
+            $_POST['bill_state'],
+            $_POST['bill_postal']
+        )
+        $user->save();
+        return $app['twig']->render('register_confirm.html.twig', array('user' => $user));
+    });
