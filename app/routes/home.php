318693eb58d375acf0293e1c86330c819864c44e
Jordan Meier <jbirdmeier@gmail.com>
2016-03-08 13:57:09 -0800

    fixing merge conflict with home route

--- app/routes/home.php
+++ app/routes/home.php
@@ -1,27 +1,18 @@
+
 <?php
-
-    $app->get('/', function()
+    $app->get('/', function() use ($app)
     {
-        return 'Hello, World!';}
-    );
-
-    $app->('/logged_in', function() use ($app){
+        return $app['twig']->render('home.html.twig');
+    });
+    $app->get('/logged_in', function() use ($app) {
         $email = $_GET['email'];
         $password = $_GET['password'];
-
-        $user_id = User::verifyPassword();
-
+        $user_id = User::passwordVerify();
         $SESSION['user'] = $user_id;
         $user = User::find($user_id);
-
         return $app['twig']->render('home.html.twig', array('name' => $user->getName()));
     });
-
-    $app->('/logged_out', function() use ($app) {
-        if(isset($_SESSION['user']))
-        {
-            unset($_SESSION['user']);
-        }
-
-        return $app['twig']->render('login.html.twig', $message => "Login to begin a session.");
+    $app->get('/logged_out', function() use ($app) {
+        session_destroy();
+        return $app['twig']->render('login.html.twig');
     });
