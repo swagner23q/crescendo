881f954bc3bf00c6147ba343adbd391a5a3de290
Blake Scott and Jordan Meier <jbirdmeier@gmail.com>
2016-03-09 14:59:56 -0800

    delete un needed routes

--- app/routes.php
+++ app/routes.php
@@ -4,11 +4,8 @@
     include ("routes/about.php");
     include ("routes/cart.php");
     include ("routes/checkout.php");
-    include ("routes/confirmation.php");
     include ("routes/login.php");
-    include ("routes/order_complete.php");
     include ("routes/product.php");
     include ("routes/products.php");
     include ("routes/profile.php");
-    include ("routes/results.php");
     include ("routes/register.php");
