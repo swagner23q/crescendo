2644f52ac99c37f339f0051be79f8a127fda2c63
Jason Awbrey <student@epicodus.com>
2016-03-09 09:43:33 -0800

    Add red stage testing phase for login route logic on get>logged_in route

--- app/routes/home.php
+++ app/routes/home.php
@@ -9,7 +9,8 @@
         $email = $_GET['email'];
         $password = $_GET['password'];
         $user_id = User::passwordVerify($email, $password);
-        if($user_id){
+
+        if($user_id !== "Wrong password! Please try again." && $user_id !== "Sorry, we don't recognize that email."){
             $_SESSION['user'] = $user_id;
             $user = User::find($user_id);
             $page_to_render = "home.html.twig";
@@ -18,9 +19,10 @@
         } else {
             $_SESSION['user'] = NULL;
             $page_to_render = "login.html.twig";
-            $error_message = "Sorry, try again!";
+            $error_message = $user_id;
             $name = FALSE;
         }
+
         var_dump($_SESSION['user']);
         return $app['twig']->render($page_to_render, array('name' => $name, 'error_message' => $error_message));
     });
