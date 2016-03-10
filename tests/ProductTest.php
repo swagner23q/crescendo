c566945152c12bd28960b55fd9ab8c31b9c65d3d
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 19:20:40 -0800

    add getall test

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -35,6 +35,35 @@
 			$this->assertEquals([$test_product], $result);
 		}
 
+		function test_getAll()
+		{
+			//Arrange
+			$gender = "M";
+			$type_id = 1;
+			$name = "Polo Button Down";
+			$description = "Light Blue button down shirt";
+			$price = 3.99;
+			$img = "/fake/path/image1.jpg";
+			$test_product = new Product($gender, $type_id, $name, $description, $price, $img);
+			$test_product->save();
+
+			$gender = "F";
+			$type_id = 2;
+			$name= "Dockers Preated Khackis";
+			$description = "Lame ass khaki pants with pleats";
+			$price = 10.99;
+			$img = "/fake/path/image2.jpg";
+			$test_product2 = new Product($gender, $type_id, $name, $description, $price, $img);
+			$test_product2->save();
+
+			//Act
+			$result = Product::getAll();
+
+			//Assert
+			$this->assertEquals([$test_product, $test_product2], $result);
+		}
+
+
 
 
 	}
