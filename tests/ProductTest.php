01d0c22f03c98bec318d91bc9edff39d9ccfec5d
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 19:09:59 -0800

    add basic product test class to product test file

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -1,1 +1,14 @@
+<?php
 
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
+
+	include ("db/db_test_connection.php");
+
+	class ProductTest extends PHPUnit_Framework_TestCase
+	{
+	}
+
+?>
