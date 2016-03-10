6e123d79414c54546de44ca45693f47d62076651
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 19:15:41 -0800

    add private properties and constructor to product class

--- src/Product.php
+++ src/Product.php
@@ -1,1 +1,23 @@
+<?php
 
+	 class Product
+	 {
+
+		private $gender;
+		private $type_id;
+		private $name;
+		private $description;
+		private $price;
+        private $img;
+		private $id;
+
+	    function __construct($gender, $type_id, $name, $description, $price, $img, $id = NULL)
+	    {
+			$this->gender = $gender;
+			$this->type_id = $type_id;
+			$this->name = $name;
+			$this->description = $description;
+			$this->price = $price;
+      		$this->img = $img;
+			$this->id = $id;
+	    }
