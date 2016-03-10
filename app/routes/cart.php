cfba18099c9c08a9e40b1073a1d80f8361df0dde
Josh Overly <joshoverly@yahoo.com>
2016-03-10 11:25:22 -0800

    add product added notification to product page

--- app/routes/cart.php
+++ app/routes/cart.php
@@ -1,24 +1,5 @@
 <?php
 
-    $app->post('/cart/add_item', function() use ($app) {
-
-      $product_id = $_POST['product_id'];
-      $qty = $_POST['qty'];
-
-      $product = Product::find($product_id);
-
-      $item = array();
-
-      array_push($item, $product_id);
-      array_push($item, $qty);
-
-      array_push($_SESSION['cart'], $item);
-
-      $total_price = Product::calculateTotalCartPrice();
-
-      return $app['twig']->render('product.html.twig', array('added' => TRUE, 'total_price' => $total_price, 'product' => $product));
-    });
-
     $app->get('/cart', function() use ($app) {
         $total_price = Product::calculateTotalCartPrice();
         return $app['twig']->render('cart.html.twig', array('total_price' => $total_price));
