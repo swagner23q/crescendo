4b39eafc9d0f2d0775bba807f8f67c74757f8714
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 19:16:26 -0800

    add getters and setters to product class

--- src/Product.php
+++ src/Product.php
@@ -21,3 +21,68 @@
       		$this->img = $img;
 			$this->id = $id;
 	    }
+	//////////id
+	    function getId()
+	    {
+	      return $this->id;
+	    }
+//////////name
+    	function getName()
+		{
+			return $this->name;
+		}
+
+		function setName($name)
+		{
+			$this->name = $name;
+		}
+/////////description
+	    function getDescription()
+	    {
+	      return $this->description;
+	    }
+
+	    function setDescription($description)
+	    {
+	      $this->description = $description;
+	    }
+///////////type_id
+	    function getTypeId()
+	    {
+	      return $this->type_id;
+	    }
+
+	    function setTypeId($type_id)
+	    {
+	      $this->type_id = $type_id;
+	    }
+////////////gender
+	    function getGender()
+	    {
+	      return $this->gender;
+	    }
+
+	    function setGender($gender)
+	    {
+	      $this->gender = $gender;
+	    }
+////////////price
+	    function getPrice()
+	    {
+	      return $this->price;
+	    }
+
+	    function setPrice($price)
+	    {
+	      $this->price = $price;
+	    }
+//////////img
+	    function getImg()
+	    {
+	      return $this->img;
+	    }
+
+	    function setImg($img)
+	    {
+	      $this->img = $img;
+	    }
