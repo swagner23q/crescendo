536647f1d2ceefe0759cf55facbdf7bf5f3c93fe
Josh Overly <joshoverly@yahoo.com>
2016-03-10 11:28:27 -0800

    clean up product route, prepare for merge

--- app/routes/product.php
+++ app/routes/product.php
@@ -1,8 +1,7 @@
 <?php
 
-    $app->get('/product', function() use ($app)
+    $app->get('/product/{id}', function($id) use ($app)
     {
-        $id = 4;
         $product = Product::find($id);
         return $app['twig']->render('product.html.twig',  array('product' => $product, 'added' => False));
     });
