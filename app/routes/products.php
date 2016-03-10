c87570b7db144e9c818a1ee591859aaf3db8114a
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 17:07:57 -0800

    add products route

--- app/routes/products.php
+++ app/routes/products.php
@@ -1,6 +1,7 @@
 <?php
 
-    $app->get('/products', function()
+    $app->get('/products', function() use ($app)
     {
-        return 'Hello, World!';}
-    );
+        $products = Product::findByTypeAndGender($search_TypeId, $search_Gender);
+        return $app['twig']->render('products.html.twig', array('products' => $products));
+    });
