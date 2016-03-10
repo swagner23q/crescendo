ed2be3877130acecefe621a0ff8ba14df7541fcc
Blake Scott and Jordan Meier <blakerscott@gmail.com>
2016-03-09 19:14:30 -0800

    add failing spec for save function

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -16,6 +16,25 @@
             Product::deleteAll();
 		}
 
+		function test_save()
+		{
+			//Arrange
+			$gender = "M";
+			$type_id = 1;
+			$name = "Polo Button Down";
+			$description = "Light Blue button down shirt";
+			$price = 3.99;
+            $img = "/fake/path/image1.jpg";
+			$test_product = new Product($gender, $type_id, $name, $description, $price, $img);
+			$test_product->save();
+
+			//Act
+			$result = Product::getAll();
+
+			//Assert
+			$this->assertEquals([$test_product], $result);
+		}
+
 
 
 	}
