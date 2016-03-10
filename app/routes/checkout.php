1b0918f2186d572ec405cf63db18c22b7eefcd6c
fight-gravity <jason.s.awbrey@gmail.com>
2016-03-10 08:40:24 -0800

    Add logic for displaying total cart price in the checkout process

--- app/routes/checkout.php
+++ app/routes/checkout.php
@@ -6,10 +6,12 @@
         {
             $variable_render = 'login.html.twig';
             $logged_in = FALSE;
+            $cart_total = FALSE;
         } else {
             $variable_render = 'checkout.html.twig';
             $logged_in = TRUE;
+            $cart_total = Product::calculateTotalCartPrice();
         }
 
-        return $app['twig']->render($variable_render, array('checkout' => TRUE, 'logged_in' => $logged_in));
+        return $app['twig']->render($variable_render, array('checkout' => TRUE, 'logged_in' => $logged_in, 'cart_total' => $cart_total));
     });
