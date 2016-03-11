2ded697f5d8f9b3d5c71f3f2a07473fc7ccbee02
Blake Scott <blakerscott@gmail.com>
2016-03-11 09:00:32 -0800

    clean up spacing in the product class

--- src/Product.php
+++ src/Product.php
@@ -1,7 +1,6 @@
 <?php
-
-	 class Product
-	 {
+	class Product
+	{
 
 		private $gender;
 		private $type_id;
@@ -21,12 +20,12 @@
       		$this->img = $img;
 			$this->id = $id;
 	    }
-	//////////id
+
 	    function getId()
 	    {
-	      return $this->id;
+	     	return $this->id;
 	    }
-//////////name
+
     	function getName()
 		{
 			return $this->name;
@@ -36,63 +35,63 @@
 		{
 			$this->name = $name;
 		}
-/////////description
+
 	    function getDescription()
 	    {
-	      return $this->description;
+	     	return $this->description;
 	    }
 
 	    function setDescription($description)
 	    {
-	      $this->description = $description;
+	     	$this->description = $description;
 	    }
-///////////type_id
+
 	    function getTypeId()
 	    {
-	      return $this->type_id;
+	     	return $this->type_id;
 	    }
 
 	    function setTypeId($type_id)
 	    {
-	      $this->type_id = $type_id;
+	     	$this->type_id = $type_id;
 	    }
-////////////gender
+
 	    function getGender()
 	    {
-	      return $this->gender;
+	     	return $this->gender;
 	    }
 
 	    function setGender($gender)
 	    {
-	      $this->gender = $gender;
+	     	$this->gender = $gender;
 	    }
-////////////price
+
 	    function getPrice()
 	    {
-	      return $this->price;
+	     	return $this->price;
 	    }
 
 	    function setPrice($price)
 	    {
-	      $this->price = $price;
+	     	$this->price = $price;
 	    }
-//////////img
+
 	    function getImg()
 	    {
-	      return $this->img;
+	     	return $this->img;
 	    }
 
 	    function setImg($img)
 	    {
-	      $this->img = $img;
+	     	$this->img = $img;
 	    }
 
 		function save()
 		{
 			$GLOBALS['DB']->exec(
-	 	   	 "INSERT INTO products
-	 	   	 (gender, type_id, name, description, price, img)
-	 	   	 VALUES
+	 	   	"INSERT INTO products
+	 	   	(gender, type_id, name, description, price, img)
+	 	   	VALUES
 				('{$this->getGender()}',
 				{$this->getTypeId()},
 				'{$this->getName()}',
@@ -101,25 +100,27 @@
 				'{$this->getImg()}'
 				)"
 			);
-	 	   	$this->id = $GLOBALS['DB']->lastInsertId();
+	 		$this->id = $GLOBALS['DB']->lastInsertId();
 		}
 
 	    static function getAll()
 		{
 			$returned_products = $GLOBALS['DB']->query("SELECT * FROM products;");
-	 	   $products = array();
-	 	   foreach($returned_products as $product) {
-			   $gender = $product['gender'];
-			   $type_id = $product['type_id'];
-	 	     $name = $product['name'];
-	 	     $description = $product['description'];
-			   $price = $product['price'];
-	       $img = $product['img'];
-			   $id = $product['id'];
-			   $new_product = new Product($gender, $type_id, $name, $description, $price, $img, $id);
-	 	       array_push($products, $new_product);
-	 	   }
-	 	   return $products;
+		 	$products = array();
+		 	foreach($returned_products as $product) {
+		    $gender = $product['gender'];
+		    $type_id = $product['type_id'];
+		    $name = $product['name'];
+		    $description = $product['description'];
+		    $price = $product['price'];
+		    $img = $product['img'];
+		    $id = $product['id'];
+
+			$new_product = new Product($gender, $type_id, $name, $description, $price, $img, $id);
+
+			array_push($products, $new_product);
+	 	}
+	 	    return $products;
 		}
 
 		static function deleteAll()
@@ -127,42 +128,40 @@
 			$GLOBALS['DB']->exec("DELETE FROM products;");
 		}
 
-		 static function find($search_id)
-         {
-             $found_product = NULL;
-             $all_products = Product::getAll();
-             foreach($all_products as $product) {
-                 if ($search_id == $product->getId()){
-                     $found_product = $product;
-                 }
-             }
-             return $found_product;
-         }
+		static function find($search_id)
+        {
+	        $found_product = NULL;
+	        $all_products = Product::getAll();
 
-		 static function findByTypeAndGender($search_TypeId, $search_Gender)
-         {
-             $found_products = array();
-             $all_products = Product::getAll();
-             foreach($all_products as $product) {
-                 if ($search_TypeId == $product->getTypeId() && $search_Gender == $product->getGender()){
-					 array_push($found_products, $product);
-                 }
-             }
+			foreach($all_products as $product) {
+		        if ($search_id == $product->getId()){
+		        $found_product = $product;
+	            }
+	        }
+            return $found_product;
+        }
 
-             return $found_products;
-         }
+		static function findByTypeAndGender($search_TypeId, $search_Gender)
+        {
+        	$found_products = array();
+        	$all_products = Product::getAll();
 
-				 static function getTypeName($search_TypeId)
-							{
-								$query = $GLOBALS['DB']->query("SELECT type from product_types where id={$search_TypeId};");
-								$some_bullshit = $query->fetchAll(PDO::FETCH_ASSOC);
-								$returned_type = $some_bullshit[0]["type"];
-								return $returned_type;
-							}
+			foreach($all_products as $product) {
+    			if ($search_TypeId == $product->getTypeId() &&
+				$search_Gender == $product->getGender()){
+				array_push($found_products, $product);
+                }
+            }
+			return $found_products;
+        }
 
-
-
-
+		static function getTypeName($search_TypeId)
+		{
+			$query = $GLOBALS['DB']->query("SELECT type from product_types where id={$search_TypeId};");
+			$fetch_all = $query->fetchAll(PDO::FETCH_ASSOC);
+			$returned_type = $fetch_all[0]["type"];
+			return $returned_type;
+	    }
 
 		function cartSave()
 		{
@@ -181,20 +180,15 @@
 
 		static function calculateCartItemPrice()
 		{
-
-
 			$cart = $_SESSION['cart'];
 			$price = 0;
 		    foreach ($cart as $item)
 		    {
-		        $product_id = $item[0];
+		    	$product_id = $item[0];
 		        $qty = $item[1];
-
-		        $product = Product::find($product_id);
-
-		        $item_price = $product->getPrice();
-
-		        $full_item_price = $item_price * $qty;
+				$product = Product::find($product_id);
+				$item_price = $product->getPrice();
+				$full_item_price = $item_price * $qty;
 		    }
 		    return $full_item_price;
 		}
@@ -208,23 +202,14 @@
 		    {
 		        $product_id = $item[0];
 		        $qty = $item[1];
-
-		        $product = Product::find($product_id);
-
-		        $item_price = $product->getPrice();
-
-		        $full_item_price = $item_price * $qty;
-
+				$product = Product::find($product_id);
+				$item_price = $product->getPrice();
+				$full_item_price = $item_price * $qty;
 				array_push($all_item_prices, $full_item_price);
 
 			}
-
 			$total_price = array_sum($all_item_prices);
 			return $total_price;
-
 		}
-
-
 	}
-
 ?>
